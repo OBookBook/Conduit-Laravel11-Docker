9afb2bf465d35b5353e69f8d613e1c586243451d
N.I <iworksWeb@gmail.com>
2024-04-29 05:32:32 +0900

    create | step1
    RealWorld のドキュメント を参考に、次のページの HTML と CSS を実装し、ページを作成してください。この時点では機能は作成せず、見た目を整えるだけでよいです。
    
    Home
    Create/Edit Article
    Article

--- api/routes/web.php
+++ api/routes/web.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
+// http://localhost:8080/
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+// http://localhost:8080/article
+Route::get('/article', function () {
+    return view('article');
+});
+
+// http://localhost:8080/create_edit_article
+Route::get('/create_edit_article', function () {
+    return view('create_edit_article');
 });
