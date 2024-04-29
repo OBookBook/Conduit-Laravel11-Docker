f0453af85af25acb30dc526b74a4c7bb095595eb
N.I <iworksWeb@gmail.com>
2024-04-30 01:44:23 +0900

    step2 | feature Home

--- api/routes/web.php
+++ api/routes/web.php
@@ -1,18 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
 
-// http://localhost:8080/
-Route::get('/', function () {
-    return view('home');
-});
-
-// http://localhost:8080/article
-Route::get('/article', function () {
-    return view('article');
-});
-
-// http://localhost:8080/create_edit_article
-Route::get('/create_edit_article', function () {
-    return view('create_edit_article');
-});
+Route::get('/', [ArticleController::class, 'index']);
